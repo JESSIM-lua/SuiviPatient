aaf8bb92f387e74634072ddcace06886831f18be
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2025-05-25 11:59:15 +0200

    fix main class

--- src/main/java/com/example/suivipatientjavafx/Main.java
+++ src/main/java/com/example/suivipatientjavafx/Main.java
@@ -9,20 +9,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), stage.getWidth(), stage.getHeight());
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400); // largeur/hauteur fixes
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-
-
-    public static void main(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), stage.getWidth(), stage.getHeight());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-
+    public static void main(String[] args) {
+        launch(args); // appelle start()
     }
 }
